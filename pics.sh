80471e365936aced01f67444a1b974ddb0a971b4
krypt <krypt@tahribat.com>
2019-05-07 00:56:19 +0300

    Initial commit

---
+++ pics.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for file in $1/*
+do
+	if [ -d $1 ]
+	then
+		continue
+	fi
+
+	create_date=$(exiftool $file | grep -m 1 'Create Date' | sed 's/  */ /g' | cut -d ' ' -f 4 | sed 's/:/_/g')
+
+	if [ ! -d $1/$create_date ]
+	then
+		mkdir $1/$create_date
+	else
+		mv $file $1/$create_date
+	fi
+			
+done
